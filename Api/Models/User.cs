61f157af32ed61b98d892a0b4d85ceb26aca7c28
wl-hsu <wlhsu.github@gmail.com>
2023-06-08 09:23:57 -0400

    setup jwt and identity added

---
+++ Api/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Api.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    }
+}
